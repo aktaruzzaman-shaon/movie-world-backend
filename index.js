f0f62a822769766dcb76340d3b68d38c74ca3389
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-12-20 23:22:22 +0600

    mongodb connection succesfully

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 5000
+
+app.use(cors());
+app.use(express.json())
+
+// mongodb integration---------------------------
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://movie-world:M4bmyeUbwFC05qJz@movie-world-1.02rq3mc.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('*',(req,res)=>{
+    res.send("no router found , 404")
+})
+app.listen(PORT,()=>{
+    console.log('Server is running')
+})
+
+
+
+
